d8b5d94eac8cf3d7629a90e05c423efd243fafa0
Roman Isko <iskoroman@gmail.com>
2018-06-24 22:07:28 +0300

    switch to standard.js code style

--- index.js
+++ index.js
@@ -1,36 +1,36 @@
-const Telegraf = require('telegraf');
-const Extra    = require('telegraf/extra');
-const bot      = new Telegraf(process.env.BOT_TOKEN);
-const rp       = require('request-promise');
+const Telegraf = require('telegraf')
+const Extra = require('telegraf/extra')
+const bot = new Telegraf(process.env.BOT_TOKEN)
+const rp = require('request-promise')
 
 bot.telegram.getMe().then((botInfo) => {
-    bot.options.username = botInfo.username
-});
+  bot.options.username = botInfo.username
+})
 
 const randomInlineButtonMarkup = Extra.HTML().markup(
-    (m) => m.inlineKeyboard([m.callbackButton('Give me a random fact', 'random')]));
+  (m) => m.inlineKeyboard([m.callbackButton('Give me a random fact', 'random')]))
 
 const options = {
-    uri : 'https://api.chucknorris.io/jokes/random',
-    json: true
-};
+  uri: 'https://api.chucknorris.io/jokes/random',
+  json: true
+}
 
 bot.command('start', (ctx) => {
-    console.log('start', ctx.from);
-    ctx.reply('Welcome!\n Bot is in development state.')
-        .then(ctx.reply('Wanna random fact about Chuck Norris?', randomInlineButtonMarkup));
-});
+  console.log('start', ctx.from)
+  ctx.reply('Welcome!\n Bot is in development state.')
+    .then(ctx.reply('Wanna random fact about Chuck Norris?', randomInlineButtonMarkup))
+})
 
 bot.action('random', (ctx) => {
-    console.log('username = ' + ctx.from.username + ' name = ' + ctx.from.first_name + ' ' + ctx.from.last_name);
-    rp(options)
-        .then(response => {
-            ctx.editMessageReplyMarkup(randomInlineButtonMarkup, undefined);
-            ctx.reply(response.value, randomInlineButtonMarkup)
-        })
-        .catch(function () {
-            ctx.reply("Error\n");
-        })
-});
+  console.log('username = ' + ctx.from.username + ' name = ' + ctx.from.first_name + ' ' + ctx.from.last_name)
+  rp(options)
+    .then(response => {
+      ctx.editMessageReplyMarkup(randomInlineButtonMarkup, undefined)
+      ctx.reply(response.value, randomInlineButtonMarkup)
+    })
+    .catch(function () {
+      ctx.reply('Error\n')
+    })
+})
 
-bot.startPolling();
+bot.startPolling()
